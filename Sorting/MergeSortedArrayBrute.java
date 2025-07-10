5efeeb2db03ce2fe586a9622a04b5c5745ce277f
BipinPal <bipinpal541@gmail.com>
2025-07-10 22:46:29 +0530

    merge sorted array problem solved

---
+++ Sorting/MergeSortedArrayBrute.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class MergeSortedArrayBrute {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);  // total O((m+n) log(m+n))
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 5, 0, 0, 0};
+        int[] nums2 = {2, 4, 6};
+        merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));  // [1, 2, 3, 4, 5, 6]
+    }
+}
