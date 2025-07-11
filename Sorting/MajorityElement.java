da3a9601e1279e0894c1087214fe145a4e8c8209
BipinPal <bipinpal541@gmail.com>
2025-07-11 22:49:03 +0530

    prblem solved by best time complexity. Moore's Algo

--- Sorting/MajorityElement.java
+++ Sorting/MajorityElement.java
@@ -32,8 +32,20 @@ public class MajorityElement{
 
         return -1; //unreachable due to assumption
     }
+
+    //method02: moore's voting algorithm
+    public static int majorityElmntMooreAlgo(int [] nums){
+        int count = 0, candidate = 0;
+
+        for(int num: nums){
+            if(count == 0) candidate = num;
+            count += (num == candidate)? 1: -1;
+        }
+        return candidate;
+    }
     public static void main(String[] args) {
         int [] nums = {3, 2, 3};
-        System.out.println(majorityElmntBrute(nums));
+        // System.out.println(majorityElmntBrute(nums));
+        System.out.println(majorityElmntMooreAlgo(nums));
     }
 }
